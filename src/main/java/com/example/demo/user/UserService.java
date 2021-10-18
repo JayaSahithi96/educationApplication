cb231b47c5be29747367323ed07bbfbbca046fb0
JayaSahithi96 <jsahithi90@gmail.com>
2021-10-18 09:33:05 -0700

    Added getUserById

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,15 +16,13 @@ public class UserService {
 		public List<User> getUser() {
 	        return userRepository.findAll();
 	    }
+		public Optional<User> findUserById(Integer id) {
+		    return userRepository.findById(id);
+		}
 	
 		public void deleteUser(Integer userId) {
-			User user = userRepository.findById(userId);
-			userRepository.delete(user);
+			userRepository.deleteById(userId);
 		}
-		public User getUserById(Integer userId) {
-			User user = userRepository.findById(userId);
-			return user;
-			
-		}
+
 	
 }
